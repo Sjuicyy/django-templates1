83d5393a224777e15421f654a542349b4eda8cfe
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-07-13 10:06:48 +0545

    temple added+context

--- product/views.py
+++ product/views.py
@@ -1,3 +1,22 @@
+from tkinter import N
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(*args,**kwargs):
+    return HttpResponse("<h1> this is home page</h1>")
+
+
+def about(request,*args,**kwargs):
+    my_context={
+        "my_text" : "my name is sammy",
+        "my_numb" : 9867867202,
+        "my_context": [123,456,789,1011,1213]
+
+    }
+    return render(request, "about.html", my_context)
+
+
+def contact(request,*args,**kwargs):
+     return render(request, "contact.html", {})
--- project1/urls.py
+++ project1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from product.views import homepage, about, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
+    path('home', homepage),
+    path('about',about),
+    path('contact',contact),
 ]
